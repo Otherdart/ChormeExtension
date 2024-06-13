753ff84343b5f6b1979527d5ee748434db0b7f52
Heiman Tong <heimanthm@gmail.com>
2024-06-13 13:26:57 +0800

    index update

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
-let choice1 = document.querySelector('.choice1');
+let choice1 = document.querySelector('#choice1');
 // let val = choose.value;
 choice1.addEventListener('click', function(){
     console.log(choice1);
 });
 
-let choice2 = document.querySelector('.choice2');
+let choice2 = document.querySelector('#choice2');
 // let val = choose.value;
 choice2.addEventListener('click', function(){
     console.log(choice2);
 });
 
-let choice3 = document.querySelector('.choice3');
+let choice3 = document.querySelector('#choice3');
 // let val = choose.value;
 choice3.addEventListener('click', function(){
     console.log(choice3);
-});
-
-let choice4 = document.querySelector('.choice4');
-// let val = choose.value;
-choice4.addEventListener('click', function(){
-    console.log(choice4);
 });
 
 
 let audioOn = document.querySelector('.swap-on');
 let audioOff = document.querySelector('.swap-off');
 let audiobtn = document.querySelector('.swap');
-const audio = new Audio('./Music/RainStromThunder.mp3');
+const audio = new Audio('./Music/Country.mp3');
+
 
 
 audioOn.addEventListener('click', function(){
 
     
     if(audiobtn = "swap-on"){
-        audio.volume = 0.2;
+        audio.volume = 0.1;
         audio.play();
         console.log(audiobtn);
-    
+
     }
 
     

ef7dcedf9991010f60d03ad0be2ebf6260fc057f
Heiman Tong <heimanthm@gmail.com>
2024-06-13 10:22:22 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+let choice1 = document.querySelector('.choice1');
+// let val = choose.value;
+choice1.addEventListener('click', function(){
+    console.log(choice1);
+});
+
+let choice2 = document.querySelector('.choice2');
+// let val = choose.value;
+choice2.addEventListener('click', function(){
+    console.log(choice2);
+});
+
+let choice3 = document.querySelector('.choice3');
+// let val = choose.value;
+choice3.addEventListener('click', function(){
+    console.log(choice3);
+});
+
+let choice4 = document.querySelector('.choice4');
+// let val = choose.value;
+choice4.addEventListener('click', function(){
+    console.log(choice4);
+});
+
+
+let audioOn = document.querySelector('.swap-on');
+let audioOff = document.querySelector('.swap-off');
+let audiobtn = document.querySelector('.swap');
+const audio = new Audio('');
+
+
+
+
+audioOn.addEventListener('click', function(){
+
+    
+    if(audiobtn = "swap-on"){
+        audio.play();
+        console.log(audiobtn);
+    
+    }
+
+    
+});
+
+audioOff.addEventListener('click', function(){
+
+    
+    if(audiobtn = "swap-off"){
+        audio.pause();
+        console.log(audiobtn);
+    
+    }
+    
+});
+
+
+// let audioOff = document.querySelector('.swap-off')
+// audioOn.addEventListener('click', function(){
+//     // console.log(audioOff);
+//     // var audio = new Audio('/Music/Rain Strom Thunder.mp3');
+//     // console.log(audio)
+//     // audio.pause();
+// });
+
+// function play() {
+//     var audio = new Audio('/Music/Rain Strom Thunder.mp3');
+//     audio.play();
+//   }
+
